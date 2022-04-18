b364ed6b4ad83109409383e20250e2d5d56f15c5
DumitruBogdan <90133050+DumitruBogdan@users.noreply.github.com>
2022-04-18 13:52:15 +0300

    Initial Commit

---
+++ Donfood/DonfoodApp.swift
@@ -0,0 +1,17 @@
+//
+//  DonfoodApp.swift
+//  Donfood
+//
+//  Created by Bogdan on 18.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DonfoodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
